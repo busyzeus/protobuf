b5157d40c7bc882099afcf5aa9d4fb3ef6627f88
Jewoo Hwang <jewoo@bluehole.net>
2016-08-25 16:50:40 +0900

    build with VS2015

--- vsprojects/config.h
+++ vsprojects/config.h
@@ -1,22 +1,35 @@
 /* protobuf config.h for MSVC.  On other platforms, this is generated
  * automatically by autoheader / autoconf / configure. */
 
-/* the location of <hash_map> */
-#define HASH_MAP_H <hash_map>
-
 /* the namespace of hash_map/hash_set */
 // Apparently Microsoft decided to move hash_map *back* to the std namespace
 // in MSVC 2010:
 //   http://blogs.msdn.com/vcblog/archive/2009/05/25/stl-breaking-changes-in-visual-studio-2010-beta-1.aspx
 // TODO(kenton):  Use unordered_map instead, which is available in MSVC 2010.
-#if _MSC_VER < 1310 || _MSC_VER >= 1600
+#if _MSC_VER < 1310 || _MSC_VER >= 1900
 #define HASH_NAMESPACE std
 #else
 #define HASH_NAMESPACE stdext
 #endif
 
-/* the location of <hash_set> */
+#if _MSC_VER >= 1900
+#define USE_UNORDERED_CONTAINER
+#endif
+
+#define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS 1
+#undef USE_UNORDERED_CONTAINER
+
+#ifdef USE_UNORDERED_CONTAINER
+#define HASH_MAP_H <unordered_map>
+#define HASH_SET_H <unordered_set>
+#define HASH_CLASS unordered_map
+#define SET_CLASS unordered_set
+#else
+#define HASH_MAP_H <hash_map>
 #define HASH_SET_H <hash_set>
+#define HASH_CLASS hash_map
+#define SET_CLASS hash_set
+#endif
 
 /* define if the compiler has hash_map */
 #define HAVE_HASH_MAP 1
